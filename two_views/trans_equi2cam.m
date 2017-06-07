979b6410b56a0549b2a10317e85c9b69c4a15c17
gain2217 <jingli@nudt.edu.cn>
2017-06-07 21:10:56 +0800

    Delete trans_equi2cam.m

--- two_views/trans_equi2cam.m
+++
@@ -1,9 +0,0 @@
-function [xcam, ycam, zcam] = trans_equi2cam(u, v, fe)
-theta = u ./ fe ;
-phi = v ./ fe;
-
-ycam = sin(phi) ;
-zcam = cos(phi) .* cos(theta) ;
-xcam = cos(phi) .* sin(theta) ;
-
-end
